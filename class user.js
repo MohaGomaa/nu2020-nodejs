0166704b1bba26402a0d9a7343f3c1d22dbef0a8
MohaGomaa <53029870+MohaGomaa@users.noreply.github.com>
2020-08-20 19:52:37 +0200

    uploading

---
+++ class user.js
@@ -0,0 +1,14 @@
+class user{
+
+constructor (username,password){
+
+    this.username=username;
+    this.password=password;
+
+}
+
+
+}
+
+let  u=new user("Mohamed","#5684");
+console.log(u.username+"/"+u.password)
