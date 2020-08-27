aa6edf54b1881042100dfd901e187002f375758c
Mohamed Ahmed Gommaa <53029870+MohaGomaa@users.noreply.github.com>
2020-08-27 18:47:15 +0200

    updated

--- online_shopDB.js
+++ online_shopDB.js
@@ -4,12 +4,12 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
 
 const productSchema = mongoose.Schema({
-    id: mongoose.Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
     name: String,
     price: Number
 });
 
-const Product = mongoose.model("Product",productSchema);
+const Product = mongoose.model("Products",productSchema);
 
 let baseURL='/products'
 
@@ -18,7 +18,7 @@ app.use(express.json());
 // get product by id
 app.get(baseURL+'/:id',function(req,res)
 {
-Product.findOne({id:req.params.id})
+Product.findOne({_id:req.params.id})
     .exec()
     .then(prod => {
         res.send("product: "+prod.name+", price: "+prod.price);
@@ -35,7 +35,7 @@ app.get(baseURL,function(req,res)
     .exec()
     .then(prod => {
         for(i=0;i<prod.length;i++)
-           res.write(prod[i].name+", id: "+prod[i].id+" price:"+prod[i].price+" ");
+           res.write(prod[i].name+", id: "+prod[i]._id+" price:"+prod[i].price+" ");
 
     })
     .catch(err => {
@@ -48,7 +48,7 @@ app.get(baseURL,function(req,res)
 app.post(baseURL,function(req,res)
 {
     const product = new Product({
-        id: new mongoose.Types.ObjectId(),
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price:req.body.price,
       });
@@ -67,7 +67,7 @@ app.put(baseURL+'/:id',function(req,res)
 {
 
 
-    Product.update({ id: req.params.id },{price:req.body.name},{price:req.body.price})
+    Product.updateOne({ _id:req.params.id },{price:req.body.name},{price:req.body.price})
     .exec()
     .then(result => {
         res.send(result);
@@ -83,7 +83,7 @@ app.put(baseURL+'/:id',function(req,res)
 app.delete(baseURL+'/:id',function(req,res)
 {
 
-    Product.remove({id:req.params.id})
+    Product.remove({_id:req.params.id})
     .exec()
     .then(result => {
         res.send(result);
